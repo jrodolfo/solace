e70cf608883d58e89fa37baf6070f19a0b67ddbc
Rod Oliveira <jrodolfo@gmail.com>
2026-05-28 20:16:15 -0300

    add javadoc for solace-broker-api.repository

--- solace-broker-api/src/main/java/org/orgname/solace/broker/api/repository/PayloadRepository.java
+++ solace-broker-api/src/main/java/org/orgname/solace/broker/api/repository/PayloadRepository.java
@@ -3,5 +3,10 @@ package org.orgname.solace.broker.api.repository;
 import org.orgname.solace.broker.api.jpa.Payload;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Repository interface for {@link Payload} entities.
+ * <p>
+ * Provides standard CRUD operations to manage message payload data.
+ */
 public interface PayloadRepository extends CrudRepository<Payload, Long> {
 }
